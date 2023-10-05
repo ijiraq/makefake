9d463525989df5c5d483da4db4246e3b9102191d
JJ Kavelaars <jjkavelaars@gmail.com>
2023-10-05 15:04:52 -0700

    Moved not ClassyMakeFake stuff elsewhere

--- classy/scripts/skyMap_pickle_info.py
+++
@@ -1,8 +0,0 @@
-import lsst, logging
-from lsst import log
-import pickle, sys
-l = log.Log()
-l.setLevel(log.Log.DEBUG)
-sm = pickle.load(open(sys.argv[1], 'rb'))
-sm.logSkyMapInfo(l)
-
--- kbmod/job.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-${HOME}/kbmod-fraserw/run.sh python ${HOME}/kbmod-fraserw/examples/run_kbmod_CFHT_scratch.py 2022-08-01 1 --input-path ./splits/splitims_1_4 --log-level INFO --reference-expnum 2773118
